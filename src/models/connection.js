180110caa5963a424cda9b32257ff858830b7574
AbdelRahman AbdelNaby <116647294+elbodzz@users.noreply.github.com>
2024-05-16 21:23:16 +0300

    first version

---
+++ src/models/connection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/jatdevdb")
+    .then(() => {
+      console.log("DB Connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+export const userModel = mongoose.model("User", userSchema);
